5c3b3eda0c3828084bd46e419c1a8d3da0d9a791
rladydqls99 <rladydqls99@naver.com>
2023-09-19 20:46:03 +0900

    styled 컴포넌트 분리

---
+++ src/pages/detail/commentStyles.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const CommentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const CommentInput = styled.textarea`
+  width: 100%;
+  height: 100px;
+  margin-bottom: 10px;
+`;
+const CommentP = styled.p`
+  width: 100%;
+  height: 100px;
+  margin-bottom: 10px;
+`;
+
+const CommentButtons = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const CommentButton = styled.button`
+  flex-grow: 1;
+  padding: 10px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+export {
+  CommentContainer,
+  CommentInput,
+  CommentP,
+  CommentButtons,
+  CommentButton,
+};
