8099ece1c3964c2a71f4049bae24ef729419e7fb
Seoyeong Min <mseo1123@naver.com>
2023-09-23 21:17:33 +0900

    마이페이지 회원정보 수정

--- src/pages/mypage/styles.js
+++ src/pages/mypage/styles.js
@@ -1,7 +1,20 @@
 import styled from "styled-components";
 
-export const Mypage = styled.div`
+export const MypageStyle = styled.div`
   width: 1170px;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Flexbox = styled.div`
+  display: flex;
   justify-content: center;
+`;
+
+export const Photobox = styled.div`
+  width: 150px;
+  height: 150px;
+  border: 1px solid black;
+  border-radius: 100px;
 `;
